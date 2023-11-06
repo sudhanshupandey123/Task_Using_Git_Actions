8c4df7c8114d6a7adce5b30088b424592c458914
Sudhanshu Pandey <65806439+sudhanshupandey123@users.noreply.github.com>
2023-11-06 16:46:22 +0530

    Update amazon_task_methods.py

--- features/steps/amazon_task_methods.py
+++ features/steps/amazon_task_methods.py
@@ -2,7 +2,7 @@ from common_ui_actions import click, get_text_value, fill_area, scroll
 
 search_box = "//input[@id='twotabsearchtextbox']"
 filtered_product_list = "//span[@class='a-size-medium a-color-base a-text-normal']"
-add_to_cart_button = "(//input[@aria-labelledby='submit.add-to-cart-announce'])[1]"
+add_to_cart_button = "(//div[@id='preAddToCartFramework_feature_div']/preceding-sibling::div[@id='addToCart_feature_div'])[2]/descendant::input[@id='add-to-cart-button']"
 
 def search_product(page, product_name):
     fill_area(page, search_box, product_name)
