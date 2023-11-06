454625dac3837addeb9b5bd22e198b9775be8d16
Sudhanshu Pandey <65806439+sudhanshupandey123@users.noreply.github.com>
2023-11-06 16:46:44 +0530

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -4,7 +4,7 @@ p = sync_playwright().start()
 
 
 def before_scenario(context, scenario):
-    context.browser = p.firefox.launch(headless=True, slow_mo=5000)
+    context.browser = p.chromium.launch(headless=True, slow_mo=5000)
     context.page = context.browser.new_page()
 
 
