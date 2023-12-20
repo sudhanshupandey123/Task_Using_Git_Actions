d8043a3b4d835425a731bcb25792db9e6b2a02e1
Sudhanshu Pandey <65806439+sudhanshupandey123@users.noreply.github.com>
2023-12-20 10:14:30 +0530

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -13,11 +13,19 @@ Project_Root=os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 def before_all(context):
     context.p = sync_playwright().start()
     path=Project_Root+"\\videos"
-    if os.path.exists(path):
+    path2=Project_Root+"\\traces"
+    if os.path.exists(path) or os.path.exists(path2):
         shutil.rmtree(path)
+        shutil.rmtree(path2)
+        path = os.path.join(Project_Root, "videos")
+        os.mkdir(path)
+        path2 = os.path.join(Project_Root, "traces")
+        os.mkdir(path2)
     else:
         path = os.path.join(Project_Root, "videos")
         os.mkdir(path)
+        path2 = os.path.join(Project_Root, "traces")
+        os.mkdir(path2)
 
 
 def before_scenario(context,scenario):
@@ -25,6 +33,8 @@ def before_scenario(context,scenario):
     context.tab = context.browser.new_context(
         record_video_dir=Project_Root+"\\videos",
         record_video_size={"width": 1500, "height": 1200}
+        # record_trace_dir = Project_Root + "\\traces",
+        # record_trace_size = {"width": 1500, "height": 1200}
     )
     context.page = context.tab.new_page()
     context.tab.tracing.start(screenshots=True, snapshots=True, sources=True)
@@ -32,10 +42,8 @@ def before_scenario(context,scenario):
 
 
 def after_scenario(context,scenario):
+    context.tab.tracing.stop(path=Project_Root+"//traces//trace.zip")
     context.page.close()
-    context.page.video.save_as(
-        f"{Project_Root}//videos/{scenario.name}.webm"
-    )
     context.page.video.save_as(os.path.join(Project_Root, f"videos/{scenario.name}"))
     with open(
             os.path.join(Project_Root, context.page.video.path()), "rb"
@@ -45,8 +53,8 @@ def after_scenario(context,scenario):
             video_file.read(),
             name=f"videos : {scenario.name}",
             attachment_type=AttachmentType.WEBM,
-        )
-    context.tab.tracing.stop(path=f"{scenario.name}_trace.zip")
+        ) 
+    attach.file(Project_Root+"//traces//trace.zip", name="Trace viewer logs", extension=".zip")
     context.tab.close()
     context.browser.close()
 
